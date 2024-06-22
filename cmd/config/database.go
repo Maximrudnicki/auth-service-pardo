88c2d79f1cf414c8001000e152de84dfc13d536b
Maxim Rudnitskiy <rydnizkui@gmail.com>
2024-06-22 19:24:44 +0300

    microservice deploy

--- cmd/config/database.go
+++ cmd/config/database.go
@@ -12,8 +12,6 @@ func ConnectionDB(config *Config) *gorm.DB {
 
 	sqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", config.DBHost, config.DBPort, config.DBUsername, config.DBPassword, config.DBName)
 
-	fmt.Println(sqlInfo)
-
 	db, err := gorm.Open(postgres.Open(sqlInfo), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
